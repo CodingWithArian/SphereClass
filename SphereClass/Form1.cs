cc2e08b3bb4db41cce20d5e00d77017c0dd8c167
Arian m <ariangsf@gmail.com>
2024-06-01 21:35:39 +0400

    Add project files.

---
+++ SphereClass/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SphereClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string output = "";
+            Sphere sphere = new Sphere();
+            double radius = int.Parse(textBox1.Text);
+            sphere.Radius = radius;
+            output += sphere.ToString();
+            output += "Perimeter : " + sphere.Perimeter() + "\n";
+            output += "Area : " + sphere.Area() + "\n";
+            output += "Valume : " + sphere.Valume() + "\n";
+            MessageBox.Show(output);
+
+            Sphere sphere1 = new Sphere(double.Parse(textBox1.Text));
+            output= sphere1.ToString();
+            MessageBox.Show(output);
+        }
+    }
+}
---
+++ SphereClass/Sphere.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SphereClass
+{
+    internal class Sphere
+    {
+        private double radius;
+
+        public double Radius
+            {
+            get { return radius; }
+            set { radius = value; }
+            }
+        public Sphere(double value)
+        {
+            radius = value;
+        }
+        public Sphere()
+        {
+            radius = 1;
+        }
+
+        public void SetRadius(double value)
+        {
+            radius = value;
+        }
+        public override string ToString()
+        {
+            return "Radius  "  + radius +"\n";
+        }
+        public double Perimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public double Area()
+        {
+            return 4 * Math.PI * Math.Pow(radius, 2);
+        }
+        public double Valume()
+        {
+            return 4 / 3.0 * Math.PI * Math.Pow(radius, 3);
+        }
+
+    }
+}
